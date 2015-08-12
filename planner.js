852cee54c9a4e287a2b5dc87225c57c46537de98
Mitch Comardo <comamitc@gmail.com>
2015-08-11 19:26:36 -0500

    2015 fixes

--- planner.js
+++ planner.js
@@ -1,50 +1,56 @@
-var excelbuilder = require('msexcel-builder');
+var excelbuilder = require('msexcel-builder-colorfix');
+var moment = require('moment');
 
 var workbook = excelbuilder.createWorkbook('./', 'planner.xlsx');
 
-var now = new Date("August 24, 2014");
-var inc = new Date("August 24, 2014");
+var now = moment("2015-08-24");//new Date("August 24, 2014");
+var inc = moment("2015-08-24");
 
 var end = 1000 * 60 * 60 * 24 * 300;
 
-while ((inc - now) < end) {
 
-  var carts = ['Colleges (28)', 'Espanol (24)', 'Elements (25)'];
+while (inc.diff(now) < end) {
 
-  inc.setDate(inc.getDate() + 1);
-  var dateString = inc.toDateString().replace(/\s+/g, '.');
+  var carts = ['Cart B (27)', 'Cart D 1&2 (33)', 'Cart C (25)', 'Ipod touch', 'Ipads (Cart 1)', 'Cart E (27)', 'Ipads (Cart 2)', 'Cart F (26)'];
+  var colors = ['e81e42', '6a308e', 'ea0fcd', 'eae23b', '4148fb', 'f0aa01', '128b4e', 'a59578'];
+  var timePeriods = 31; // number of 15 minute time periods in one day
+  var timeSlot = 15; // 15 minute time slots
 
-  console.log(dateString);
+  if (inc.day() > 0 && inc.day() < 6) {
+    var dateString = inc.format("ddd.MMM.D.YY");
+    var sheet = workbook.createSheet(dateString, carts.length * 3, timePeriods);
 
-  var len = 31,
-    mins = 15;
+    console.log("saving for date " + dateString);
 
-  if (inc.getDay() > 0 && inc.getDay() < 6) {
-    var sheet = workbook.createSheet(dateString, 9, len);
     for (var j = 0; j < carts.length; j++) {
-
+      var dataRow = 3;
       var col = (j * 3) + 1;
-      sheet.set(col, 1, carts[j]);
-      sheet.set(col + 1, 2, 'Teacher');
+      console.log(carts[j]);
+      sheet.set(col, 1, carts[j]); // set cart name
+      sheet.set(col + 1, 2, 'Teacher'); // column header
       sheet.set(col + 2, 2, 'Reason');
 
-      var dataRow = 3;
+      // fill with a color
+      sheet.fill(col, 1, {'type': 'solid', 'fgColor': colors[j]});
+      sheet.fill(col, 2, {'type': 'solid', 'fgColor': colors[j]});
 
-      inc.setHours(8);
-      inc.setMinutes(0);
-      inc.setSeconds(0);
+      inc.hours(8);
+      inc.minutes(0);
+      inc.seconds(0);
 
-      for (var i = dataRow; i <= len; i++) {
-        sheet.set(col, i, inc.toLocaleTimeString("en-US"));
-        inc.setMinutes(inc.getMinutes() + 15);
-      }
+      for(var i = dataRow; i <= timePeriods; i++) {
+        sheet.set(col, i, inc.format("hh:mm a"));
+        sheet.fill(col, i, {'type': 'solid', 'fgColor': colors[j]});
+        inc.minute(inc.minute() + timeSlot);
+      } 
     }
   }
+  inc.add(1, 'd');
 }
 
-workbook.save(function(ok) {
-  if (!ok) {
-    workbook.cancel();
+workbook.save(function(err) {
+  if (err) {
+    console.log(err);
   } else {
     console.log('congratulations, your workbook created');
   }
